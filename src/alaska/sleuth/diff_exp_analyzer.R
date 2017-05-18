d33224a79c76a38f6fe5d03efbf8054325801cac
Joseph Min <kmin@caltech.edu>
2017-05-18 16:40:13 -0700

    removed genotype hardcoding
    takes genotype values from rna_seq_info.txt

--- src/alaska/sleuth/diff_exp_analyzer.R
+++ src/alaska/sleuth/diff_exp_analyzer.R
@@ -1,23 +1,23 @@
-library("sleuth")
-library("optparse")
-library("files")
+library('sleuth')
+library('optparse')
+library('files')
 
 # command line arguments
 option_list <- list(
-  make_option(c("-v", "--verbose"), action="store_true", default=TRUE,
-              help="Print extra output [default]"),
-  make_option(c("-d", "--directory"), type='character', default=character(0),
-              help="Please specify the directory of the matrix"),
-  make_option(c("-o", "--output"), type='character', default=character(0),
-              help="Please specify the output directory"),
-  make_option(c("-ge", "--genovar"), type='character', default=character(0),
-              help="Please specify the genotype variable name"),
-  make_option(c("-ba", "--batch"), action="store_true", default=FALSE,
-              help="Batch correction method"),
-  make_option(c("-bv", "--batchvar"), type='character', default=character(0),
-              help="Please specify the batch variable name"),
-  make_option(c("-s", "--shiny"), action='store_true', default=FALSE,
-              help="Command to open shiny console")
+  make_option(c('-v', '--verbose'), action='store_true', default=TRUE,
+              help='Print extra output [default]'),
+  make_option(c('-d', '--directory'), type='character', default=character(0),
+              help='Please specify the directory of the matrix'),
+  make_option(c('-o', '--output'), type='character', default=character(0),
+              help='Please specify the output directory'),
+  make_option(c('-ge', '--genovar'), type='character', default=character(0),
+              help='Please specify the genotype variable name'),
+  make_option(c('-ba', '--batch'), action='store_true', default=FALSE,
+              help='Batch correction method'),
+  make_option(c('-bv', '--batchvar'), type='character', default=character(0),
+              help='Please specify the batch variable name'),
+  make_option(c('-s', '--shiny'), action='store_true', default=FALSE,
+              help='Command to open shiny console')
 )
 
 opt = parse_args(OptionParser(option_list=option_list))
@@ -35,11 +35,11 @@ if(!file.exists(opt$d)) {
 }
 
 #gene info for sleuth
-print("#Fetching bioMart info (1/2)")
-mart <- biomaRt::useMart(biomart = "ensembl", dataset = "celegans_gene_ensembl")
-print("#Fetching bioMart info (2/2)")
-t2g <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id",
-                                     "external_gene_name"), mart = mart)
+print('#Fetching bioMart info (1/2)')
+mart <- biomaRt::useMart(biomart = 'ensembl', dataset = 'celegans_gene_ensembl')
+print('#Fetching bioMart info (2/2)')
+t2g <- biomaRt::getBM(attributes = c('ensembl_transcript_id', 'ensembl_gene_id',
+                                     'external_gene_name'), mart = mart)
 print('#Renaming genes')
 t2g <- dplyr::rename(t2g, target_id = ensembl_transcript_id,
                      ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
@@ -53,12 +53,16 @@ output_dir <- opt$o
 
 #get ids
 print('#Reading analysis matrix')
-sample_id <- dir(file.path(base_dir, "results"))
+sample_id <- dir(file.path(base_dir, 'results'))
 print(sample_id)
-kal_dirs <- sapply(sample_id, function(id) file.path(base_dir, "results", id, "kallisto"))
+kal_dirs <- sapply(sample_id, function(id) file.path(base_dir, 'results', id, 'kallisto'))
 print(kal_dirs)
-s2c <- read.table(file.path(base_dir, "rna_seq_info.txt"), header = TRUE, stringsAsFactors= FALSE)
+s2c <- read.table(file.path(base_dir, 'rna_seq_info.txt'), header = TRUE, stringsAsFactors= FALSE)
 print(s2c)
+
+# Unique, sorted genotype list
+genotypes <- sort(unique(s2c[, 'genotype']))
+print(genotypes)
 
 print('#Reading Kallisto results')
 s2c <- dplyr::select(s2c, sample= experiment, genotype, batch)
@@ -78,46 +82,37 @@ so <- sleuth_lrt(so, 'reduced', 'full')
 
 #Wald test implementations
 #no interactions
-print('#Computing Wald test on betas')
-so <- sleuth_wt(so, which_beta = 'genotypebB', which_model = 'full')
-so <- sleuth_wt(so, which_beta = 'genotypebA', which_model = 'full')
-so <- sleuth_wt(so, which_beta = 'genotypebC', which_model = 'full')
-so <- sleuth_wt(so, which_beta = 'genotypebD', which_model = 'full')
-so <- sleuth_wt(so, which_beta = 'genotypebE', which_model = 'full')
-so <- sleuth_wt(so, which_beta = 'genotypebF', which_model = 'full')
-so <- sleuth_wt(so, which_beta = 'genotypebG', which_model = 'full')
+for (genotype in genotypes) {
+  # Ignore WT geneotype
+  if (!grepl('WT', genotype)) {
+    # String for current progress
+    progress <- paste('(', match(genotype, genotypes)-1, '/', length(genotypes)-1, ')')
+    print(paste('#Computing Wald test on genotype', substring(genotype, 2), progress))
+
+    so <- sleuth_wt(so, which_beta = paste('genotype', genotype), which_model = 'full')
+  }
+}
 
 
-#write results to tables
-print('#Writing results to file')
-results_table <- sleuth_results(so, 'genotypebB', 'full', test_type= 'wt')
-write.csv(results_table, paste(output_dir, 'betasB.csv', sep='/'))
-results_table <- sleuth_results(so, 'genotypebA', 'full', test_type= 'wt')
-write.csv(results_table, paste(output_dir, 'betasA.csv', sep='/'))
-results_table <- sleuth_results(so, 'genotypebC', 'full', test_type= 'wt')
-write.csv(results_table, paste(output_dir, 'betasC.csv', sep='/'))
-results_table <- sleuth_results(so, 'genotypebD', 'full', test_type= 'wt')
-write.csv(results_table, paste(output_dir, 'betasD.csv', sep='/'))
-results_table <- sleuth_results(so, 'genotypebE', 'full', test_type= 'wt')
-write.csv(results_table, paste(output_dir, 'betasE.csv', sep='/'))
-results_table <- sleuth_results(so, 'genotypebF', 'full', test_type= 'wt')
-write.csv(results_table, paste(output_dir, 'betasF.csv', sep='/'))
-results_table <- sleuth_results(so, 'genotypebG', 'full', test_type= 'wt')
-write.csv(results_table, paste(output_dir, 'betasG.csv', sep='/'))
+#write results to csv
+for (genotype in genotypes) {
+  if(!grepl('WT', genotype)) {
+    # '(current index/total length)'
+    progress <- paste('(', match(genotype, genotypes)-1, '/', length(genotypes)-1, ')')
 
-#so <- sleuth_wt(so, which_beta = '(Intercept)', which_model = 'full')
-#so <- sleuth_wt(so, which_beta = batchvar, which_model = 'full')
+    # 'betasX.csv'
+    output_file <- paste('betas', substring(genotype, 2), '.csv', sep='')
 
-#results_table <- sleuth_results(so, '(Intercept)','full', test_type= 'wt')
-#write.csv(results_table, paste(output_dir, 'intercept.csv', sep='/'))
-
-#results_table <- sleuth_results(so, batchvar,'full', test_type= 'wt')
-#write.csv(results_table, paste(output_dir, 'batch.csv', sep='/'))
+    print(paste('#Writing genotype', substring(genotype, 2), 'results to', output_file, progress))
+    results_table <- sleuth_results(so, paste('genotype', genotype), 'full', test_type='wt')
+    write.csv(results_table, paste(output_dir, output_file, sep='/'))
+  }
+}
 
 sr <- sleuth_results(so, 'reduced:full', 'lrt')
 write.csv(sr, paste(output_dir, 'batch_lrt.csv', sep='/'))
 
-print("#Starting shiny web server")
 if (opt$shiny) {
+  print('#Starting shiny web server')
   sleuth_live(so)
 }
