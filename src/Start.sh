a40b5e10c1b6b329274c21d22f5bac841a73809f
Joseph Min <kmin@caltech.edu>
2018-07-26 16:58:02 -0700

    Start.sh now checks if all required Docker images, containers, and volumes are correctly installed before attempting to start the server.

--- src/Start.sh
+++ src/Start.sh
@@ -1,10 +1,75 @@
 #!/bin/bash
 # This script starts AlaskaServer
 
-container_name="alaska"
+####### DEFINE VARIABLES #######
+# Docker image tags.
+DOCKER_SCRIPT_VOLUME="alaska_script_volume"
+DOCKER_DATA_VOLUME="alaska_data_volume"
+DOCKER_ALASKA_TAG="alaska"
+DOCKER_REQUEST_TAG="alaska_request"
+DOCKER_QC_TAG="alaska_qc"
+DOCKER_KALLISTO_TAG="alaska_kallisto"
+DOCKER_SLEUTH_TAG="alaska_sleuth"
+DOCKER_CGI_TAG="alaska_cgi"
+
+# Declare array varables for required images, containers, and volumes.
+declare -a images=(
+    "$DOCKER_ALASKA_TAG"
+    "$DOCKER_REQUEST_TAG"
+    "$DOCKER_QC_TAG"
+    "$DOCKER_KALLISTO_TAG"
+    "$DOCKER_SLEUTH_TAG"
+    "$DOCKER_CGI_TAG"
+)
+declare -a containers=(
+    "$DOCKER_ALASKA_TAG"
+    "$DOCKER_CGI_TAG"
+)
+declare -a volumes=(
+    "$DOCKER_SCRIPT_VOLUME"
+    "$DOCKER_DATA_VOLUME"
+)
+####### END VARIABLE DEFINITIONS #######
+
+# Make sure all the required images are present.
+for val in "${images[@]}"
+do
+    if [[ $(docker images -q $val) == "" ]]
+    then
+        printf "%s\n" "Docker image '$val' is not correctly installed."
+        printf "%s\n" "Please run Setup.sh to correctly set up Alaska."
+        exit 1
+    fi
+done
+
+# Make sure all the required containers are present.
+for val in "${containers[@]}"
+do
+    if [[ $(docker ps -a -f "name=$val" --format '{{.Names}}') != $val ]]
+    then
+        printf "%s\n" "Docker container '$val' is not correctly installed."
+        printf "%s\n" "Please run Setup.sh to correctly set up Alaska."
+        exit 1
+    fi
+done
+
+# Make sure all the required volumes are present.
+for val in "${volumes[@]}"
+do
+    if [[ $(docker volume ls -f "name=$val" --format '{{.Name}}') != $val ]]
+    then
+        printf "%s\n" "Docker volume '$val' is not correctly installed."
+        printf "%s\n" "Please run Setup.sh to correctly set up Alaska."
+        exit 1
+    fi
+done
+
+
+#### Now, we can ensure Alaska was properly installed.
+# Let's go on to start the server.
 
 # First, check if the container is already running.
-if [ $(docker inspect -f '{{.State.Running}}' $container_name) = "true" ]
+if [[ $(docker inspect -f '{{.State.Running}}' $DOCKER_ALASKA_TAG) == "true" ]]
 then
     printf "%s\n" "Alaska is already running. What would you like to do?"
     printf "\t%s\n" "1. Attach to the stdout of the container that is currently running."
@@ -13,13 +78,13 @@ then
     printf "\t%s\n" "4. Exit this script without doing anything."
     read -p ">" choice
     case "$choice" in
-        1 ) docker attach $container_name;;
+        1 ) docker attach $DOCKER_ALASKA_TAG;;
         2 ) sudo cp -a scripts/* /var/lib/docker/volumes/alaska_script_volume/_data/
-            docker restart $container_name;;
-        3 ) docker stop $container_name;;
+            docker restart $DOCKER_ALASKA_TAG;;
+        3 ) docker stop $DOCKER_ALASKA_TAG;;
         * ) ;;
     esac
 else
     sudo cp -a scripts/* /var/lib/docker/volumes/alaska_script_volume/_data/
-    docker start -i $container_name
+    docker start -i $DOCKER_ALASKA_TAG
 fi
