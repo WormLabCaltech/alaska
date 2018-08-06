776822d35a71de0243946dbc64c357c6589960e9
Joseph Min <kmin@caltech.edu>
2018-08-06 12:03:32 -0700

    added script for sending commands to the server from the cgi

---
+++ src/scripts/cgi_request.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script sends a new request to the AlaskaServer
+COMMAND="$1"
+ID="$2 $3"
+
+# Set up variables from set_env_variables.sh
+source set_env_variables.sh
+
+docker run -i --rm --network="container:$DOCKER_ALASKA_TAG" \
+                -v $DOCKER_SCRIPT_MOUNT \
+                -w "/alaska/scripts" \
+                alaska_request\
+                python AlaskaRequest.py ${COMMAND} ${ID}
