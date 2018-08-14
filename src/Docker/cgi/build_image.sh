45278ee62c0fef71f6e56a347162bb6fda70d3f0
Joseph Min <phoenixter96@gmail.com>
2018-08-13 22:26:27 -0700

    set cgi docker to have python installed

--- src/Docker/cgi/build_image.sh
+++ src/Docker/cgi/build_image.sh
@@ -4,7 +4,11 @@
 source scripts/set_env_variables.sh
 
 # build cgi image
-docker build -t $DOCKER_CGI_TAG Docker/cgi/
+docker build -t $DOCKER_CGI_TAG \
+             --build-arg MINICONDA_VER="$MINICONDA_VER" \
+             --build-arg MINICONDA3_URL="$MINICONDA3_URL" \
+             --no-cache \
+             Docker/cgi/
 
 # exit with return value of the above command
 exit $?
