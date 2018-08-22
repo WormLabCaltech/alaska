9581789170839d2d1d1a34248f3092e8c16582d0
Joseph Min <kmin@caltech.edu>
2018-08-22 15:35:07 -0700

    sleep for testing

--- src/web/html/cgi_request.php
+++ src/web/html/cgi_request.php
@@ -14,6 +14,8 @@ function cgi_request($action, $id) {
     # now, run the command and get output
     $out = shell_exec($cmd);
 
+    sleep(5);
+
     # simply spit out the output
     echo $out;
 }
