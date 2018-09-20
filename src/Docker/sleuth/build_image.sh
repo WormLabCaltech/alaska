c790cd67eab276d5be39507b71fb3b32b4ae6978
Joseph Min <phoenixter96@gmail.com>
2018-09-20 10:24:58 -0700

    sleuth image to include tea

--- src/Docker/sleuth/build_image.sh
+++ src/Docker/sleuth/build_image.sh
@@ -4,6 +4,8 @@ source scripts/set_env_variables.sh
 
 # build request image
 docker build -t $DOCKER_SLEUTH_TAG \
+             --build-arg MINICONDA_VER="$MINICONDA_VER" \
+             --build-arg MINICONDA3_URL="$MINICONDA3_URL" \
              --build-arg SLEUTH_VER="$SLEUTH_VER" \
              --no-cache \
              Docker/sleuth/
