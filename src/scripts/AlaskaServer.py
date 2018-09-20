5e12e85ab256fb398ed523043475acfd8b1047a9
Joseph Min <phoenixter96@gmail.com>
2018-09-20 14:00:17 -0700

    lock test

--- src/scripts/AlaskaServer.py
+++ src/scripts/AlaskaServer.py
@@ -493,7 +493,7 @@ class AlaskaServer(Alaska):
 
         response = [to, msg]
         # acquire lock
-        lock = threading.Lock()
+        lock = threading.RLock()
         if (lock.acquire()):
             self.SOCKET.send_multipart(response)
             lock.release()
