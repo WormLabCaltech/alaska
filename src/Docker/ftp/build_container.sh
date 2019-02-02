dbd59d67212e23def92826d1ad16f0c290656538
Lioscro <kmin@caltech.edu>
2019-02-01 16:44:04 -0800

    add quotes for environment variables that have spaces

--- src/Docker/ftp/build_container.sh
+++ src/Docker/ftp/build_container.sh
@@ -9,9 +9,9 @@ docker create -it --name="$DOCKER_FTP_TAG" \
               -v $DOCKER_FTP_MOUNT \
               -p $DOCKER_FTP_PORT \
               -p $DOCKER_FTP_PORTS \
-              -e $DOCKER_FTP_ENV1 \
-              -e $DOCKER_FTP_ENV2 \
-              -e $DOCKER_FTP_ENV3 \
+              -e "$DOCKER_FTP_ENV1" \
+              -e "$DOCKER_FTP_ENV2" \
+              -e "$DOCKER_FTP_ENV3" \
               --restart unless-stopped \
               $DOCKER_FTP_BASE
 
