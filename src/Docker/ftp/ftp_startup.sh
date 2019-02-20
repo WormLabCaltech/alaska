70ff1cee7fcbc44039ff7b6cd1873bea5286570e
Lioscro <kmin@caltech.edu>
2019-02-20 13:05:27 -0800

    removed deprecated ftp docker files

--- src/Docker/ftp/ftp_startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script is run when the ftp container is started.
-
-# FTP mount must be owned by the ftpgroup.
-chown -R ftpuser.ftpgroup /alaska/root
-
-# Then, run ftp server
-/run.sh -l puredb:/etc/pure-ftpd/pureftpd.pdb -E -j -R -P alaska.caltech.edu
